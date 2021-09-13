a05af8ac2fa94f860272266331a5dc9637ff01b6
BioSniper <biosniper@gmail.com>
2021-09-13 16:15:39 +0100

    Initial commit
    Initial commit of twitchbanbot

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# banbot
- 
+# twitchbanbot
+Very early version of a banbot to simply run the ban commands in chat to ban bots provided in the original project https://github.com/therealnagia/twitchbanfromlist
+This bot grabs the text file from the same directory as the bot, and iterates the ban commands in your own chat line by line.
+Built from the example twitch bot API code at; https://dev.twitch.tv/docs/irc
+
+##Requirements
+nodejs
+ tmi.js - Twitch IRC Chat (Twitch Messaging Interface)
+ dotenv - for loading environment variables
+ line-reader - For processing the files
+
+##Rate limits
+Rate limits set as per; https://dev.twitch.tv/docs/irc/guide#rate-limits
+100 messages per 30 seconds max (can ban 1 user every 0.5sec to be safe)
+You should not chat at the same time as running the bans from this bot as you may hit rate limits.
+
+Hitting rate limits can result in a perma ban!!
+
+##Warranty
+**There is literally no warranty on this bot.** Read and check the code for yourself before running it.
+**If you are unsure, dont run it.**
+**If you get banned this is NOT my fault in any way.** You were warned it was possible!!
+**Any damage, losses caused from use of this code, death, war, famine, natural disaster, etc (outcomes are not limited to this list specifically, bascially anything that happens because you ran this..) is not my responsibility.**
+
+##BEFORE RUNNING:
+Generate an oauth token from https://twitchapps.com/tmi/
+Edit the variables in example.env to your requirements for user and oauth token
+Variables must remain inside of the quote marks
+When happy with your config rename the file to .env (remove the word example from the file)
+From your directory run: node bot.js
---
+++ bot.js
@@ -0,0 +1,88 @@
+// This bot has been made from lots of example code to serve a single purpose of banning multiple names from a list
+// This bot conforms to Twitch Bot rate limits on the 13/09/2021
+// See https://dev.twitch.tv/docs/irc/guide#rate-limits
+//
+// ------Command and message rate limits 
+// ------If command and message rate limits are exceeded, an application cannot send chat messages or commands for 30 minutes.
+// ------100 per 30 seconds
+// ------Users sending commands or messages to channels in which they are the broadcaster or have Moderator status
+
+// Your .env file should specify an account you are using as a moderator in your channel
+// I would recommend having a second account specifically for this purpose
+// If you chat in your channel at the same time as the bot is running, you MAY hit the message limit rate
+// This could result in your account being banned!
+
+const tmi = require('tmi.js');
+const dotenv = require('dotenv');
+// Define configuration options
+
+dotenv.config({path: '.env'});
+
+const opts = {
+  identity: {
+    username: process.env.BOT_USERNAME, // Username for bot account
+    password: process.env.OAUTH_TOKEN // Should include the full token including the oauth:
+  },
+  channels: [
+    process.env.CHANNEL_NAME
+  ]
+};
+
+// Create a client with our options
+const client = new tmi.client(opts);
+
+// Register our event handlers (defined below)
+client.on('message', onMessageHandler);
+client.on('connected', onConnectedHandler);
+
+// Connect to Twitch:
+client.connect();
+
+// Called every time a message comes in
+function onMessageHandler (target, context, msg, self) {
+  if (self) { return; } // Ignore messages from the bot
+
+  // Remove whitespace from chat message
+  const commandName = msg.trim();
+  
+
+  // If the command is known, let's execute it
+  if (commandName === '!nose') {
+    client.say(target, 'Starting banning run in 5 seconds...');
+    console.log('* Executed ${commandName} command');
+    console.log('Ban run starting in 5 seconds...');
+    console.log('You can stop this ban run at any time by Killing the nodejs process running this script');
+    sleep(5000);
+    console.log('Ban run begins...');
+    client.say(target, 'Starting now... Check nodejs console for output!');
+    readbans();
+   
+  } else {
+    console.log('* Unknown command ${commandName}');
+  }
+}
+
+
+// Called every time the bot connects to Twitch chat
+function onConnectedHandler (addr, port) {
+  console.log('* Connected to ${addr}:${port}');
+}
+
+function readbans (lineReader, target, msg, self, context) {
+var lineReader = require('line-reader');
+    lineReader.eachLine('banlist.txt', function(line) {
+    client.say(target, '/ban ' + line);
+    console.log('Banned user ' + line);
+    sleep(500); // Twitch Documentation indicates max 100 messages per 30 seconds for mods or broadcasters. 0.5sec between bans keeps us within this time limit nicely. Go faster and your account may get banned!
+    if (line.includes('BANLISTEND')) { return false; }
+});
+}
+
+function sleep(milliseconds) {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
+}
+
